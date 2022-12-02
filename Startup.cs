86e31d70dfb1f6dd03da57188dec4f70903563b3
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2022-12-02 09:55:48 -0500

    Nuevo paquete Microsoft.AspNetCore.Mvc.NewtonsoftJson v3.1.31

--- Startup.cs
+++ Startup.cs
@@ -20,7 +20,7 @@ namespace ProyectoFrontBack
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
 
             services.AddDbContext<InspeccionDbContext>(
                 options => options.UseSqlServer("name=ConnectionStrings:myConnection"));
